2559564ee5d65c79636548bb5887683ebb75ee25
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-08 10:35:13 -0500

    models/state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
-class State(BaseModel):
+class State(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
     """ State class """
-    name = ""
+    if HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable = False)
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            from models.city import City
+            from models import storage
+
+            city_objects = storage.all(City)
+            return [city for city in city_objects.values() if city.state_id == self.id]
