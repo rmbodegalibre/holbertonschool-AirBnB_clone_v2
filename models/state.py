bc5e46f8799f612c1e4d7970c1bf4997dd702ef5
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-08 10:42:56 -0500

    models/state.py 02

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base if HBNB_TYPE_STORAGE == 'db' else object):
     if HBNB_TYPE_STORAGE == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable = False)
-
+        cities = relationship("City", backref="state", cascade="delete")
     else:
         name = ""
 
